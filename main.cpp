039bb06178e1070dc7bff24abc75e0be87ef8df8
Zelak7561 <bs0057082@gmail.com>
2025-04-01 21:45:31 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int col = 0;
+    int rig = 0;
+
+    cout << "Inserire le colonne: " << endl;
+    cin >> col;
+    cout << "Inserire le righe: " << endl;
+    cin >> rig;
+
+    int **m = new int*[rig] ;
+    // Array allocato dinamicamente che contiene tutti gli indirizzi relative alle righe
+
+    for (int i = 0; i < col; i++) {
+        m[i] = new int[col];
+        for (int j = 0; j < rig; j++) {
+            int r = 0;
+            cout << "Inserire il numero: " << endl;
+            cin >> r;
+            m[i][j] = r;
+        }
+    }
+
+
+    for (int i = 0; i < col; i++) {
+        for (int j = 0; j < rig; j++) {
+            cout << "Numero | " << m[i][j] << endl;
+        }
+    }
+
+    for (int i = 0; i < col; i++) {
+        delete[] m[i];
+    }
+
+    delete[] m;
+    /*
+    Differenza tra delete[] e delete:
+     -delete[], elimina l'intero array nel nostro caso andremo
+      a dealocare ogni singola riga della matrice;
+      utilizzato nelle casistiche es -> (int m = new int[10], un array di 10 interi)
+     -delete, dealoca la singola variabile es -> (int m = new int, ovvero una singola variabile)
+     */
+
+
+    return 0;
+}
